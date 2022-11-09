0b887add0fbe136ac822053e357b76aa5214fd0c
am22ar <amarpatil12222@gmail.com>
2022-11-09 18:46:50 +0530

    Added: UC-7

---
+++ src/com/java/hotelreservation/Hotel.java
@@ -0,0 +1,80 @@
+package com.java.hotelreservation;
+
+public class Hotel {
+	 public String hotelName;
+	    public int weekdayRateForRegular;
+	    public int weekendRateForRegular;
+	    public int weekdayRateForRewarder;
+	    public int weekendRateForRewarder;
+	    public int hotelRatings;
+
+	    public Hotel(String hotelName, int weekdayRateForRegular, int weekendRateForRegular, int weekdayRateForRewarder, int weekendRateForRewarder, int hotelRatings) {
+	        this.hotelName = hotelName;
+	        this.weekdayRateForRegular = weekdayRateForRegular;
+	        this.weekendRateForRegular = weekendRateForRegular;
+	        this.weekdayRateForRewarder = weekdayRateForRewarder;
+	        this.weekendRateForRewarder= weekendRateForRewarder;
+	        this.hotelRatings = hotelRatings;
+	    }
+
+	    public void setHotelName(String hotelName) {
+	        this.hotelName = hotelName;
+	    }
+
+	    public String getHotelName() {
+	        return hotelName;
+	    }
+
+	    public int getWeekdayRateForRegularCustomer() {
+	        return weekdayRateForRegular;
+	    }
+
+	    public void setWeekdayRateForRegularCustomer(int weekdayRate) {
+	        this.weekdayRateForRegular = weekdayRate;
+	    }
+
+	    public int getWeekendRateForRegularCustomer() {
+	        return weekendRateForRegular;
+	    }
+
+	    public void setWeekendRateForRegularCustomer(int weekendRate) {
+	        this.weekendRateForRegular = weekendRate;
+	    }
+
+
+	    public int getWeekdayRateForRewarder() {
+	        return weekdayRateForRewarder;
+	    }
+
+	    public void setWeekdayRateForRewarder(int weekdayRateForRewarder) {
+	        this.weekdayRateForRewarder = weekdayRateForRewarder;
+	    }
+
+	    public int getWeekendRateForRewarder() {
+	        return weekendRateForRewarder;
+	    }
+
+	    public void setWeekendRateForRewarder(int weekendRateForRewarder) {
+	        this.weekendRateForRewarder = weekendRateForRewarder;
+	    }
+
+	    public int getHotelRatings() {
+	        return hotelRatings;
+	    }
+
+	    public void setHotelRatings(int hotelRatings) {
+	        this.hotelRatings = hotelRatings;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "\n" + "Hotel{" +
+	                "HotelName='" + hotelName + '\'' +
+	                ", WeekdayRateForRegular=" + weekdayRateForRegular + '\'' +
+	                ",WeekendRateForRewarder+" + weekendRateForRegular +
+	                ", WeekdayRateForRegular=" + weekdayRateForRewarder + '\'' +
+	                ",WeekendRateForRewarder+" + weekendRateForRewarder +
+	                ",hotelRatings+" + hotelRatings +
+	                '}';
+	    }
+}
---
+++ src/com/java/hotelreservation/HotelReservationTest.java
@@ -0,0 +1,24 @@
+package com.java.hotelreservation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HotelReservationTest {
+	 @Test
+	    public void givenHotelNameShouldBePassed() {
+	        HotelReservation hotelReservation = new HotelReservation();
+
+	        boolean lakewoodHotel = hotelReservation.checkHotelDetails(new Hotel("LakeWoods", 110,80,90, 80, 3));
+	        Assert.assertTrue(lakewoodHotel);
+	        boolean bridgewoodHotel = hotelReservation.checkHotelDetails(new Hotel("Bridgewood", 160,110,60, 50, 4));
+	        Assert.assertTrue(bridgewoodHotel);
+	        boolean ridgewoodhotel = hotelReservation.checkHotelDetails(new Hotel("Ridgewood", 220,110, 150, 40, 5));
+	        Assert.assertTrue(ridgewoodhotel);
+	    }
+
+	    @Test
+	    public void toFindBestRatingHotel() {
+	        HotelReservation hotelReservation = new HotelReservation();
+	        hotelReservation.getBestRatingsHotel();
+	    }
+}
